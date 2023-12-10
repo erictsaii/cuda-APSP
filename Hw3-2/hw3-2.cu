7182eedce27f4f972596589721d41538cda92f82
erictsaii <erictsai90@gmail.com>
2023-12-10 22:08:48 +0800

    complete phase 2

--- Hw3-2/hw3-2.cu
+++ Hw3-2/hw3-2.cu
@@ -58,7 +58,7 @@ void output(const char* filename) {
 	fclose(file);
 }
 
-__global__ void phase_1(int *d_D, int round) {
+__global__ void phase_1(int *d_D, int round, int V) {
     __shared__ int share_D[B * B];
     // for share
     const int s_x = threadIdx.x;
@@ -67,27 +67,96 @@ __global__ void phase_1(int *d_D, int round) {
     const int g_x = round * B + threadIdx.x; 
     const int g_y = round * B + threadIdx.y; 
     // block size is 64*64 but we only have 32*32 threads 
+    // 1 thread deals with 4 kinds of D[i, j]
     share_D[s_y * B + s_x] = d_D[g_y * V + g_x];
-    share_D[(s_y + half_B) * B + s_x] = d_D[(g_y + half_B) * V + g_x];
     share_D[s_y * B + (s_x + half_B)] = d_D[g_y * V + (g_x + half_B)];
+    share_D[(s_y + half_B) * B + s_x] = d_D[(g_y + half_B) * V + g_x];
     share_D[(s_y + half_B) * B + (s_x + half_B)] = d_D[(g_y + half_B) * V + (g_x + half_B)];
 
     __syncthreads();
 
-    #pragma unroll 32
+    #pragma unroll 32 //necessary?
 	for (int k = 0; k < B; ++k) {
 		share_D[s_y * B + s_x] = min(share_D[s_y * B + s_x], share_D[s_y * B + k] + share_D[k * B + s_x]);
+        share_D[s_y * B + (s_x + half_B)] = min(share_D[s_y * B + (s_x + half_B)], share_D[s_y * B + k] + share_D[k * B + (s_x + half_B)]);
 		share_D[(s_y + half_B) * B + s_x] = min(share_D[(s_y + half_B) * B + s_x], share_D[(s_y + half_B) * B + k] + share_D[k * B + s_x]);
-		share_D[s_y * B + (s_x + half_B)] = min(share_D[s_y * B + (s_x + half_B)], share_D[s_y * B + k] + share_D[k * B + (s_x + half_B)]);
 		share_D[(s_y + half_B) * B + (s_x + half_B)] = min(share_D[(s_y + half_B) * B + (s_x + half_B)], share_D[(s_y + half_B) * B + k] + share_D[k * B + (s_x + half_B)]);
 		__syncthreads();
 	}
 
     // load back to global
 	d_D[g_y * V + g_x] = share_D[s_y * B + s_x];
-    d_D[(g_y + half_B) * V + g_x] = share_D[(s_y + half_B) * B + s_x];
     d_D[g_y * V + (g_x + half_B)] = share_D[s_y * B + (s_x + half_B)]; 
+    d_D[(g_y + half_B) * V + g_x] = share_D[(s_y + half_B) * B + s_x];
     d_D[(g_y + half_B) * V + (g_x + half_B)] = share_D[(s_y + half_B) * B + (s_x + half_B)];
+}
+
+__global__ void phase_2(int *d_D, int round, int V) {
+    if (blockIdx.x == round) return;
+    // init share memory
+    __shared__ int pivot_D[B * B];
+    __shared__ int row_D[B * B];
+    __shared__ int col_D[B * B];
+    // load pivot_D
+    const int s_x = threadIdx.x;
+    const int s_y = threadIdx.y;
+    int g_x = round * B + threadIdx.x; 
+    int g_y = round * B + threadIdx.y;
+
+    pivot_D[s_y * B + s_x] = d_D[g_y * V + g_x];
+    pivot_D[s_y * B + (s_x + half_B)] = d_D[g_y * V + (g_x + half_B)];
+    pivot_D[(s_y + half_B) * B + s_x] = d_D[(g_y + half_B) * V + g_x];
+    pivot_D[(s_y + half_B) * B + (s_x + half_B)] = d_D[(g_y + half_B) * V + (g_x + half_B)];
+    
+    // load row_D
+    g_x = blockIdx.x * B + threadIdx.x; 
+    g_y = round * B + threadIdx.y;
+
+    row_D[s_y * B + s_x] = d_D[g_y * V + g_x];
+    row_D[s_y * B + (s_x + half_B)] = d_D[g_y * V + (g_x + half_B)];
+    row_D[(s_y + half_B) * B + s_x] = d_D[(g_y + half_B) * V + g_x];
+    row_D[(s_y + half_B) * B + (s_x + half_B)] = d_D[(g_y + half_B) * V + (g_x + half_B)];
+
+    // load col_D
+    g_x = round * B + threadIdx.x; 
+    g_y = blockIdx.x * B + threadIdx.y;
+
+    col_D[s_y * B + s_x] = d_D[g_y * V + g_x];
+    col_D[s_y * B + (s_x + half_B)] = d_D[g_y * V + (g_x + half_B)];
+    col_D[(s_y + half_B) * B + s_x] = d_D[(g_y + half_B) * V + g_x];
+    col_D[(s_y + half_B) * B + (s_x + half_B)] = d_D[(g_y + half_B) * V + (g_x + half_B)];
+
+    __syncthreads();
+
+    // calculate
+    #pragma unroll 32
+    for (int k = 0; k < B; ++k){
+        // row
+        row_D[s_y * B + s_x] = min(row_D[s_y * B + s_x], pivot_D[s_y * B + k] + row_D[k * B + s_x]);
+        row_D[s_y * B + (s_x + half_B)] = min(row_D[s_y * B + (s_x + half_B)], pivot_D[s_y * B + k] + row_D[k * B + (s_x + half_B)]);
+		row_D[(s_y + half_B) * B + s_x] = min(row_D[(s_y + half_B) * B + s_x], pivot_D[(s_y + half_B) * B + k] + row_D[k * B + s_x]);
+		row_D[(s_y + half_B) * B + (s_x + half_B)] = min(row_D[(s_y + half_B) * B + (s_x + half_B)], pivot_D[(s_y + half_B) * B + k] + row_D[k * B + (s_x + half_B)]);
+        // col
+        col_D[s_y * B + s_x] = min(col_D[s_y * B + s_x], pivot_D[s_y * B + k] + col_D[k * B + s_x]);
+        col_D[s_y * B + (s_x + half_B)] = min(col_D[s_y * B + (s_x + half_B)], pivot_D[s_y * B + k] + col_D[k * B + (s_x + half_B)]);
+		col_D[(s_y + half_B) * B + s_x] = min(col_D[(s_y + half_B) * B + s_x], pivot_D[(s_y + half_B) * B + k] + col_D[k * B + s_x]);
+		col_D[(s_y + half_B) * B + (s_x + half_B)] = min(col_D[(s_y + half_B) * B + (s_x + half_B)], pivot_D[(s_y + half_B) * B + k] + col_D[k * B + (s_x + half_B)]);
+        // no __syncthreads()?
+    }
+    // load col back to global
+    d_D[g_y * V + g_x] = col_D[s_y * B + s_x];
+    d_D[g_y * V + (g_x + half_B)] = col_D[s_y * B + (s_x + half_B)];
+    d_D[(g_y + half_B) * V + g_x] = col_D[(s_y + half_B) * B + s_x];
+    d_D[(g_y + half_B) * V + (g_x + half_B)] = col_D[(s_y + half_B) * B + (s_x + half_B)];
+
+    // load row back to global
+    g_x = blockIdx.x * B + threadIdx.x; 
+    g_y = round * B + threadIdx.y;
+
+    d_D[g_y * V + g_x] = row_D[s_y * B + s_x];
+    d_D[g_y * V + (g_x + half_B)] = row_D[s_y * B + (s_x + half_B)];
+    d_D[(g_y + half_B) * V + g_x] = row_D[(s_y + half_B) * B + s_x];
+    d_D[(g_y + half_B) * V + (g_x + half_B)] = row_D[(s_y + half_B) * B + (s_x + half_B)];
 }
 
 int main(int argc, char** argv) {
@@ -106,12 +175,12 @@ int main(int argc, char** argv) {
     // block
     // B 32 or 64?
     int rounds = V / B;
-    dim3 block(rounds, rounds);
-    dim3 num_threads(B, 1024 / B); //?
-    dim3 phase_1_num_threads(32, 32);
+    dim3 num_threads(32, 32); // maybe 64*16 is faster 
+    dim3 phase_2_blocks(rounds, 1);
     
     for (int i = 0; i < rounds; ++i) {
-        phase_1<<<1, phase_1_num_threads>>>(d_D, i);
+        phase_1<<<1, num_threads>>>(d_D, i, V);
+        phase_2<<<phase_2_blocks, num_threads>>>(d_D, i, V);
     }
 
     // output
